0c8bb33bc0333ef2d0712d89b50cea3e3eb1e531
Ignacio Sanchez Gines <nacho@geardome.com>
2018-07-17 23:36:50 +0200

    authentication via Azure cli

--- demo.tf
+++ demo.tf
@@ -1,8 +1,4 @@
 provider "azurerm" {
-  client_id       = "${var.azure_client_id}"
-  client_secret   = "${var.azure_client_secret}"
-  subscription_id = "${var.azure_subscription_id}"
-  tenant_id       = "${var.azure_tenant_id}"
 }
 
 resource "azurerm_resource_group" "demo" {
